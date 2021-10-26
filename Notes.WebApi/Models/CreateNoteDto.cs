b408ce5bb9a9ecd98db93cce19a1a4f525d37588
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-10-26 20:46:09 +0600

    Создание WebApi

---
+++ Notes.WebApi/Models/CreateNoteDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Notes.Commands.CreateNote;
+using AutoMapper;
+
+namespace Notes.WebApi.Models
+{
+    public class CreateNoteDto:IMapWith<CreateNoteCommand>
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateNoteDto, CreateNoteCommand>()
+                .ForMember(noteCommand => noteCommand.Title,
+                opt => opt.MapFrom(noteDto => noteDto.Title))
+                .ForMember(noteCommand => noteCommand.Details,
+                opt => opt.MapFrom(noteDto => noteDto.Details));
+        }
+
+
+    }
+}
--- Notes.WebApi/Startup.cs
+++ Notes.WebApi/Startup.cs
@@ -7,18 +7,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Application;
+using System.Reflection;
+using Notes.Persistence;
+using Microsoft.Extensions.Configuration;
 
 namespace Notes.WebApi
 {
+
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(config => {
+                config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+                config.AddProfile(new AssemblyMappingProfile(typeof(INoteDbContext).Assembly));
+            });
+            services.AddApplication();
+            services.AddPersistence(Configuration);
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyHeader();
+                });
+            });
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,13 +55,12 @@ namespace Notes.WebApi
             }
 
             app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
